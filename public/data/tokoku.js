737879b33a8082e782d4e36cdb22cd74173d4277
bintang <bntnglaila@gmail.com>
2024-10-14 20:46:18 +0700

    first commit

---
+++ public/data/tokoku.js
@@ -0,0 +1,22 @@
+{
+  [
+    {
+      idproduk: "P001",
+      kodeproduk: "KTSHIRT001",
+      namaproduk: "Kaos Polos Putih",
+      hargamodal: "50000",
+      hargajual: "75000",
+      hargadiskon: "70000",
+      stok: "100",
+    },
+    {
+      idproduk: "P002",
+      kodeproduk: "KTSHIRT002",
+      namaproduk: "Kaos Polos Hitam",
+      hargamodal: "50000",
+      hargajual: "75000",
+      hargadiskon: "70000",
+      stok: "150",
+    },
+  ];
+}
